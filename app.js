fac0bad2bf0c6b3ff3d630cee9fb0ae738f579ca
kieranfarr <kieran.farr@gmail.com>
2016-03-18 14:17:21 -0700

    basic working with profileid

--- app.js
+++ app.js
@@ -7,38 +7,56 @@ var bodyParser = require('body-parser');
 var swig = require('swig');
 var passport = require('passport');
 var TwitterStrategy = require('passport-twitter').Strategy;
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+function Player(connection, id) {
+    this.connection = connection;
+    this.id = id;
+    this.position = {x: 0, y: 0, z: 0};
+    this.orientation = {x: 0, y: 0, z: 0, w: 1};
+    this.kills = 0;
+    this.deaths = 0;
+    this.sendCommand = function(command) {
+        this.connection.sendUTF(JSON.stringify(command));
+    };
+    return this;
+}
+
+players = {}
+
 // view engine setup
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
 
-// passport - sessions - twitter
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-passport.use(new TwitterStrategy({
-    consumerKey: "Abv0qwzcRUkqCU9sPDl0BB2rB",
-    consumerSecret: "",
-    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
-  },
-  function(token, tokenSecret, profile, cb) {
-    User.findOrCreate({ twitterId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
-
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  console.log("serializeUser requested, user = " + user);
+  players[user] = new Player(null, user);
+  done(null, user);
+});
+
+passport.deserializeUser(function(id, done) {
+  console.log("deserializeUser requested, id = " + id);
+  done(null, players[id]);
+//  User.findById(id, function(err, user) {
+//    done(err, user);
+//  });
+});
 
 // routes
 app.use('/', routes);
@@ -56,16 +74,16 @@ app.get('/auth/twitter/callback',
   });
 
 
-  // Define routes.
-  app.get('/',
-    function(req, res) {
-      res.render('home', { user: req.user });
-    });
+// Define routes.
+app.get('/',
+  function(req, res) {
+    res.render('home', { user: req.user });
+});
 
-  app.get('/login',
-    function(req, res){
-      res.render('login');
-    });
+app.get('/login',
+  function(req, res){
+    res.render('login');
+});
 
   app.get('/login/twitter',
     passport.authenticate('twitter'));
@@ -82,6 +100,22 @@ app.get('/auth/twitter/callback',
       res.render('profile', { user: req.user });
     });
 
+
+// passport - sessions - twitter
+passport.use(new TwitterStrategy({
+    consumerKey: "Abv0qwzcRUkqCU9sPDl0BB2rB",
+    consumerSecret: "",
+    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, cb) {
+//    players.findOrCreate( twitterId: profile.id);
+    console.log("New user profile id = " + profile.id);
+    return cb(null, profile.id);
+    // User.findOrCreate({ twitterId: profile.id }, function (err, user) {
+    //   return cb(err, user);
+    // });
+  }
+));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
